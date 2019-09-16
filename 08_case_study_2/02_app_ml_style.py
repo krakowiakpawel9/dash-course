32f9383f18f50d804518d3c010ea288aa3f4f84b
krakowiakpawel9 <krakowiakpawel9@gmail.com>
2019-09-16 15:55:04 +0200

    build frontend

--- 08_case_study_2/02_app_ml_style.py
+++ 08_case_study_2/02_app_ml_style.py
@@ -3,26 +3,20 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
 from dash.dependencies import Input, Output
-import pickle
 import pandas as pd
+import pickle
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+df = pd.read_csv('./datasets/data_cleaned.csv', index_col=0)
 
 with open('model.pickle', 'rb') as file:
     model = pickle.load(file)
-
-df = pd.read_csv('./datasets/data_cleaned.csv', index_col=0)
-
-X = df.copy()
-y = X.pop('Price')
-
-print(model.predict(X.iloc[:1, :]))
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     html.Div([
-        html.H3('Model Uczenia Maszynowego - Regresyjny Model Przewidywania Ceny Samochodów Używanych'),
+        html.H3('Model Uczenia Maszynowego - Regersyjny Model Przewidywania Ceny Samochodów Używanych'),
         html.H6('Model Lasów Losowych (biblioteka scikit-learn)')
     ], style={'textAlign': 'center'}),
     html.Hr(),
@@ -33,10 +27,9 @@ app.layout = html.Div([
             min=df.Year.min(),
             max=df.Year.max(),
             step=1,
-            marks={i: str(i) for i in range(df.Year.min(), df.Year.max() + 1)},
-            tooltip={'placement': 'bottom'}
+            marks={i: str(i) for i in range(df.Year.min(), df.Year.max() + 1)}
         ),
-        html.Br(),
+        html.Hr(),
         html.Label('Podaj rozmiar silnika:'),
         dcc.Slider(
             id='slider-2',
@@ -46,7 +39,7 @@ app.layout = html.Div([
             marks={i: str(i) for i in range(0, 6001, 500)},
             tooltip={'placement': 'bottom'}
         ),
-        html.Br(),
+        html.Hr(),
         html.Label('Podaj moc samochodu:'),
         dcc.Slider(
             id='slider-3',
@@ -61,48 +54,44 @@ app.layout = html.Div([
         html.Div([
             dcc.Dropdown(
                 id='dropdown-1',
-                options=[
-                    {'label': i, 'value': i} for i in [2, 4, 5, 6, 7, 8, 9, 10]
-                ]
+                options=[{'label': i, 'value': i} for i in [2, 4, 5, 6, 7, 8, 9, 10]]
             )
         ], style={'width': '20%', 'textAlign': 'left'}),
         html.Br(),
-        html.Label('Podaj typ paliwa:'),
+        html.Label('Podaj typ paliwa'),
         html.Div([
             dcc.Dropdown(
                 id='dropdown-2',
-                options=[
-                    {'label': i, 'value': j} for i, j in zip(['Diesel', 'Benzyna', 'CNG', 'LPG', 'Elektryczny'],
-                                                             ['Diesel', 'Petrol', 'CNG', 'LPG', 'Electric'])
-                ]
+                options=[{'label': i, 'value': j} for i, j in zip(['Diesel', 'Benzyna', 'CNG', 'LPG', 'Elektryczny'],
+                                                                  ['Diesel', 'Petrol', 'CNG', 'LPG', 'Electric'])]
             )
         ], style={'width': '20%', 'textAlign': 'left'}),
         html.Br(),
-        html.Label('Podaj typ przekładni:'),
+        html.Label('Podaj typ przekładni'),
         html.Div([
             dcc.RadioItems(
                 id='radio-1',
                 options=[{'label': i, 'value': j} for i, j in zip(['Manualna', 'Automatyczna'],
                                                                   ['Manual', 'Automatic'])]
             )
-        ]),
+        ], style={'width': '20%', 'textAlign': 'left'}),
+
         html.Div([
             html.Hr(),
             html.H3('Predykcja na podstawie modelu'),
             html.Hr(),
-            html.H4('Podałeś następujące parametry:'),
+            html.H4('Podałeś parametry:'),
             html.Div(id='div-1'),
             html.Div(id='div-2'),
             html.Hr()
         ], style={'margin': '0 auto', 'textAlign': 'center'})
+
     ], style={'width': '80%', 'textAlign': 'left', 'margin': '0 auto', 'fontSize': 22,
               'background-color': 'white', 'padding': '30px'})
 ], style={'background-color': '#61BAAE'})
 
-fuel_type = {'Diesel': 'Diesel', 'Petrol': 'Benzyna', 'CNG': 'CNG',
-             'LPG': 'LPG', 'Electric': 'Elektryczny'}
+fuel_type = {'Diesel': 'Diesel', 'Petrol': 'Benzyna', 'CNG': 'CNG', 'LPG': 'LPG', 'Electric': 'Elektryczny'}
 transmission = {'Manual': 'Manualna', 'Automatic': 'Automatyczna'}
-
 
 @app.callback(
     Output('div-1', 'children'),
@@ -119,10 +108,9 @@ def display_parameters(val1, val2, val3, val4, val5, val6):
         val6 = transmission[val6]
         return html.Div([
             html.H6(f'Rok produkcji: {val1}'),
-            html.H6(dcc.Markdown(f'Pojemność silnika: {val2} cm<sup>3</sup>',
-                                 dangerously_allow_html=True)),
-            html.H6(f'Moc silnika: {val3} KM'),
-            html.H6(f'Liczba pasażerów: {val4}'),
+            html.H6(f'Pojemność silnika: {val2}'),
+            html.H6(f'Moc silnika: {val3}'),
+            html.H6(f'Liczba Pasażerów: {val4}'),
             html.H6(f'Typ paliwa: {val5}'),
             html.H6(f'Typ przekładni: {val6}')
         ], style={'textAlign': 'left'})
@@ -130,7 +118,6 @@ def display_parameters(val1, val2, val3, val4, val5, val6):
         return html.Div([
             html.H6('Podaj wszystkie parametry!')
         ])
-
 
 @app.callback(
     Output('div-2', 'children'),
@@ -160,7 +147,7 @@ def predict_value(val1, val2, val3, val4, val5, val6):
         else:
             val6 = 0
 
-        df = pd.DataFrame(
+        df_sample = pd.DataFrame(
             data=[
                 [val1, val2, val3, val4, val5_1, val5_2, val5_3, val5_4, val6]
             ],
@@ -168,19 +155,15 @@ def predict_value(val1, val2, val3, val4, val5, val6):
                      'Fuel_Type_Electric', 'Fuel_Type_LPG', 'Fuel_Type_Petrol',
                      'Transmission_Manual']
         )
-        print(df)
+        print(df_sample)
 
-        price = model.predict(df)[0]
+        price = model.predict(df_sample)[0]
         price = round(price * 1000, 2)
+
         return html.Div([
-            html.H3(f'Sugerowana cena ${price}')
-        ], style={
-            'background-color': '#FF467C',
-            'height': '40px',
-            'width': '60%',
-            'margin': '0 auto',
-            'vertical-align': 'middle'
-        })
+            html.H4(f'Sugerowana cena ${price}')
+        ], style={'background-color': '#AF90C2', 'width': '60%', 'margin': '0 auto'})
+
 
 
 if __name__ == '__main__':
